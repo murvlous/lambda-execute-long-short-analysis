392f942c07cc77e6cc9c9b059ba7b7f9c05e3e4e
Murilo <by_murilo@hotmail.com>
2022-03-02 13:18:08 -0300

    consulta no dynamodb

--- lambda-execute-analysis/handler.js
+++ lambda-execute-analysis/handler.js
@@ -4,6 +4,7 @@ const fetch = require("node-fetch");
 const { env } = require("process");
 const aws = require('aws-sdk')
 const sns = new aws.SNS({ region: 'sa-east-1' })
+const ddb = new aws.DynamoDB({apiVersion: '2012-08-10', region: 'sa-east-1'});
 
 async function publishSnsTopic (subject, data) {
   const params = {
@@ -18,13 +19,32 @@ exports.handler = async (event, context, callback) => {
 
   console.log(event)
 
+  const flg_dose_reforco =  event.flg_dose_reforco || false
+
+  const paramsConsultaDb = {
+    TableName: "mark47_ativos_monitorados",
+  };
+
+  ddb.scan(paramsConsultaDb, function (err, data) {
+    if (err) {
+      console.log("Error on retrieving data from mark47_ativos_monitorados DynamoDb table", err);
+    } else {
+      console.log("Success on retrieving data from mark47_ativos_monitorados DynamoDb table");
+      data.Items.forEach(function (element, index, array) {
+        console.log(element);
+      });
+    }
+  });
+
   const PETR4_response = await fetch('https://api.hgbrasil.com/finance/stock_price?key=dcf12d06&symbol=PETR4');
   const PETR4_data = await PETR4_response.json();
   const PETR4_price = PETR4_data.results.PETR4.price
+  const PETR4_horario = PETR4_data.results.PETR4.updated_at
 
   const PETR3_response = await fetch('https://api.hgbrasil.com/finance/stock_price?key=dcf12d06&symbol=PETR3');
   const PETR3_data = await PETR3_response.json();
   const PETR3_price = PETR3_data.results.PETR3.price
+  const PETR3_horario = PETR3_data.results.PETR3.updated_at
 
   //Avaliando Long Petr4 x Short Petr3
 
@@ -68,7 +88,9 @@ exports.handler = async (event, context, callback) => {
     const message = `
     Avaliando Operação Long Petr3 x Short Petr4
     Preço PETR4 = ${PETR4_price}
+    Horário de atualização PETR4 = ${PETR4_horario}
     Preço PETR3 = ${PETR3_price}
+    Horário de atualização PETR3 = ${PETR3_horario}
     Fator PETR4/PETR3 calculado = ${PETR4_price / PETR3_price}
     Configurado para ativar se fator <= 1.2850
     Sinal de Entrada ATIVADO
